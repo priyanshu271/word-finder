e3d094433c485214520dfcb264f4b35cbaef57b8
Priyanshu <priyanshurai271@gmail.com>
2024-01-09 20:50:01 +0530

    feat: added selector itself in app html to deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { SearchWordComponent } from './components/search-word/search-word.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,SearchWordComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
